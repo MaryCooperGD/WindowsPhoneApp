e7f289850f7ad52d8cc6a8996e2a3b0e537bde4a
BlackBlade <luca.longobardi.93@gmail.com>
2015-08-11 17:06:21 +0200

    First vs test commit

--- WindowsPhoneApp/MainPage.xaml.cs
+++ WindowsPhoneApp/MainPage.xaml.cs
@@ -44,7 +44,7 @@ namespace WindowsPhoneApp
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
 
-            //prova 
+            //lumaca
         }
     }
 }
