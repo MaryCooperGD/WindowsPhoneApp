b8b13d434551eb12c1a28396e4115c8863abb27e
BlackBlade <luca.longobardi.93@gmail.com>
2015-08-23 20:02:43 +0200

    Finished utility classes used by the external database. Next commit will feature the db interaction.

--- WindowsPhoneApp/RegistrationManager.cs
+++ WindowsPhoneApp/RegistrationManager.cs
@@ -6,16 +6,14 @@ using System.Threading.Tasks;
 
 namespace WindowsPhoneApp
 {
-    public static class RegistrationManager
+    public class RegistrationManager
     {
-        /* Tags for the dictionary. Maybe an enumeration is better*/
-        public static readonly string Monday = "mon";
-        public static readonly string Tuesday = "tue";
-        public static readonly string Wednesday = "wed";
-        public static readonly string Thursday = "thu";
-        public static readonly string Friday = "fri";
-        public static readonly string Saturday = "sat";
-        public static readonly string Sunday = "sun";
+        private RegistrationManager manager = new RegistrationManager();
+
+        public static enum DayOfWeek
+        {
+            MON,TUE,WED,THU,FRI,SAT,SUN 
+        }
         
 
         /*Save data*/
@@ -27,10 +25,17 @@ namespace WindowsPhoneApp
         public static string description;
         public static double Lat;
         public static double Lng;
-        public static IDictionary<string, TimeSpan> daytime = new Dictionary<string,TimeSpan>(); /* Instead of timespan, we need a custom class to assign 4 times for each day */
+        private IDictionary<DayOfWeek, DayTimespan> daytime = new Dictionary<DayOfWeek,DayTimespan>();
 
         /*Private, singleton pattern*/
         private RegistrationManager() { }
+
+        public RegistrationManager getInstance()
+        {
+            return this.manager;
+        }
+
+        
     
     }
 }
