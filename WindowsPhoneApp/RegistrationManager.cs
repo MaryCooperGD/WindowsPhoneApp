8f41dc46b7489406e57370270d193d58ec3f413d
BlackBlade <luca.longobardi.93@gmail.com>
2015-10-21 15:23:12 +0200

    Revert "Project finished."
    This reverts commit d51e80bd62b10393ba2e2d69ac0c2b594d8f6823.

--- WindowsPhoneApp/RegistrationManager.cs
+++ WindowsPhoneApp/RegistrationManager.cs
@@ -14,7 +14,7 @@ namespace WindowsPhoneApp
     {
         private static RegistrationManager manager = new RegistrationManager();
 
-        public static readonly string ParseName = "account_2";
+        public static readonly string ParseName = "account_1";
 
         public enum DayOfWeek
         {
